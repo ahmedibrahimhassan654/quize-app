8f1d46ab046a7b9d60c78372d171771390aba4cd
Mohamed Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-02-21 00:38:23 +0200

    get result score

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,32 +1,26 @@
 import React ,{useState} from 'react'
 
- const QuestionBox = ({question,options,nextquestion,questionId}) => {
+ const QuestionBox = ({question,options,selected,nextquestion,questionId}) => {
    const [answer,setAnswer]=useState(options)
     return (
   
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            {question}
-          </div>
-      
+    
+          <div className="questionBox">
+              <div className='question'>
+              {question}
+              </div>
+    
         {answer.map((text,index)=>(
             <button key={index} className="answerBtn" onClick={()=>{
                 setAnswer([text])
+                selected(text)
             }}>
                         {text}
             </button>
         ))}
         </div>
-        {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button> */}
-      </div>
+    
+      
      
     )
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import QuestionBox from './components/QuestionBox'
 class QuizBee extends Component{
     state={
         questionBank:[],
-        singleQuestion:{}
+        singleQuestion:{},
+        score:0,
+        response:0
+
 
     }
     getQuestions=()=>{
@@ -25,6 +28,16 @@ class QuizBee extends Component{
            
         })
     }
+    computeAnswer=(answer,correctAnser)=>{
+        if(answer===correctAnser){
+            this.setState({
+                score:this.state.score+1
+            })
+        }
+        this.setState({
+            response:this.state.response<5?this.state.response+1:5
+        })
+    }
     componentDidMount(){
         this.getQuestions()
     }
@@ -36,12 +49,19 @@ class QuizBee extends Component{
                     
                 </div>
                 {this.state.questionBank.length>0&&
-                    this.state.questionBank.map(({question,answers,correct,questionId,singleQuestion})=>
+                this.state.response<5&&
+                    this.state.questionBank.map(({question,answers,correct,questionId})=>
                     
                     (
-                 <QuestionBox question={question} options={answers} key={questionId}/>
+                 <QuestionBox 
+                 question={question} 
+                 options={answers} 
+                 key={questionId}
+                 selected={answer=>this.computeAnswer(answer,correct)}
+                 />
                     )
                     )}
+                    {this.state.response===5?<h2>{this.state.score}</h2>:null}
 
             </div>
         )
