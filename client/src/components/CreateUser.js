bc7e44033dd021459407e65fe3b5a725de6b516c
Mohamed Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-02-21 06:18:54 +0200

    refactor my code

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,18 +1,31 @@
 import React ,{useState} from 'react'
 import { Link ,useHistory} from 'react-router-dom';
-
+import QuizBee from "./QuizBee";
 const CreateUser=()=> {
 
     const [user,setuser]=useState('')
 
     let history = useHistory();
 
-    const handleSubmit=(event)=>{
+	
+    
+ 
+
+
+    const handleSubmit= async(event)=>{
+        event.preventDefault();
         // console.log('Success:', value);
         alert('A name was submitted: ' + user);
-    event.preventDefault();
-        // <Link to='/startexam'> start exam</Link>
+    try {
+        //create this user in database
+
+
         history.push('/startexam')
+    } catch (error) {
+        console.log(error);
+    }
+       
+        
     }
     
     const handleChange=(event)=>{
@@ -25,26 +38,29 @@ const CreateUser=()=> {
     return (
      
          <div className='container '>
-    <form className="position-absolute top-50 start-50 translate-middle"
-        onSubmit={handleSubmit}
-         >
-        <div className="mb-3">
-        <label for="exampleInputName" className="form-label">Your name please</label>
-        <input type="text" 
-        className="form-control" 
-        id="exampleInputName" 
-        placeholder="your name"
-        value={user}
-        onChange={handleChange}
-        />
-        
-        </div>
-
+             {
+                 <form className="position-absolute top-50 start-50 translate-middle"
+                 onSubmit={handleSubmit}
+                  >
+                 <div className="mb-3">
+                 <label for="exampleInputName" className="form-label">Your name please</label>
+                 <input type="text" 
+                 className="form-control" 
+                 id="exampleInputName" 
+                 placeholder="your name"
+                 value={user}
+                 onChange={handleChange}
+                 />
+                 
+                 </div>
+         
+             
+             <button type="submit" className="btn btn-primary" 
+             // onClick={}
+             >Submit</button>
+             </form>
+             }
     
-    <button type="submit" className="btn btn-primary" 
-    // onClick={}
-    >Submit</button>
-    </form>
 </div>
       
 
